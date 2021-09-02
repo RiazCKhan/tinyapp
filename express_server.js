f3fa5e632ea21e1d885143ebe086f1cb1e042c17
RiazK <RiazCKhan@gmail.com>
2021-09-02 15:13:12 -0400

    Implemented POST register new users && Updated  user templateVars

--- express_server.js
+++ express_server.js
@@ -15,27 +15,26 @@ const randomGenerator = generateRandomString => {
   return Math.random().toString(16).substr(2, 6);
 }
 
-const users = {
-  'user1RandomID': {
-    id: randomGenerator(),
-    email: 'user1@example.com',
-    password: '123456'
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
   },
-  'user2RandomID': {
-    id: randomGenerator(),
-    email: 'user2@example.com',
-    password: 'strong2gether!'
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
   }
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-
 app.get("/", (req, res) => {
-  res.redirect("/urls")
+  res.redirect("/register")
 });
 
 app.get("/urls.json", (req, res) => {
@@ -43,19 +42,19 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] }; // updated w/username
+  const templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"]}; // updated w/username
+  const templateVars = { user: users[req.cookies['user_id']] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-  const templateVars = { shortURL, longURL, username: req.cookies["username"] }; // updated w/username
+  const templateVars = { shortURL, longURL, user: users[req.cookies['user_id']] };
   res.render("urls_show", templateVars);
 });
 
@@ -76,7 +75,8 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("regLogin")  
+  const templateVars = { user: req.cookies["user_id"] };
+  res.render("register", templateVars)
 });
 
 // ---------------------------------- POST BELOW
@@ -111,9 +111,23 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/urls");
 });
+
+app.post("/register", (req, res) => {
+  const randomID = randomGenerator() 
+  const { email, password } = req.body
+  const user = { 
+    id: randomID,
+    email,
+    password
+  };
+  users[randomID] = user
+  res.cookie('user_id', randomID)
+  res.redirect("/urls")
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

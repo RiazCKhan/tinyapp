a1d4b0d3d62cf464dc2562bfca0d2421a3f25298
RiazK <RiazCKhan@gmail.com>
2021-09-03 11:11:11 -0400

    removed getUserByEmail function

--- functionAid.js
+++ functionAid.js
@@ -3,14 +3,4 @@ const randomGenerator = generateRandomString => {
   return Math.random().toString(16).substr(2, 6);
 }
 
-const getUserByEmail = (email) => {
-  for (const id in users) {
-    const user = users[id];
-    if (user.email === email) {
-      return user;
-    }
-  }
-  return null;
-};
-
-module.exports = { randomGenerator, getUserByEmail }
+module.exports = { randomGenerator }

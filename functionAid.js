a5c46f26dc2c2cd3f8464ebce29ed3da50b230e0
RiazK <RiazCKhan@gmail.com>
2021-09-03 18:35:20 -0400

    Modified function to increase randomization

--- functionAid.js
+++ functionAid.js
@@ -1,6 +1,6 @@
 // Generate random alphanumeric characters
 const randomGenerator = generateRandomString => {
-  return Math.random().toString(16).substr(2, 6);
+  return Math.random().toString(36).substr(2, 6);
 }
 
 module.exports = { randomGenerator }

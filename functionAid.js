2e2eb0a2382dadf7519c5a4428f2b98f8b7901b1
RiazK <RiazCKhan@gmail.com>
2021-09-04 19:24:35 -0400

    removed urlsForUser function

--- functionAid.js
+++ functionAid.js
@@ -1,18 +1,6 @@
-const { urlDatabase, users } = require('./express_server')
-
 // Generate random alphanumeric characters
 const randomGenerator = generateRandomString => {
-  return Math.random().toString(36).substr(2, 6);
+  return Math.random().toString(16).substr(2, 6);
 }
 
-const urlsForUser = function (user_id) {
-  const urlsBelongingToUser = {};
-  for (url in urlDatabase) {
-    if (user_id === urlDatabase[url].userID) {
-      urlsBelongingToUser[url] = urlDatabase[url];
-    }
-  }
-  return urlsBelongingToUser;
-}
-
-module.exports = { randomGenerator, urlsForUser }
+module.exports = { randomGenerator }

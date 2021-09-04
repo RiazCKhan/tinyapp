2c207a5142c427e17bb127a474dc668a7a17148b
RiazK <RiazCKhan@gmail.com>
2021-09-04 18:48:01 -0400

    Added urlsForUser helper function

--- functionAid.js
+++ functionAid.js
@@ -1,6 +1,18 @@
+const { urlDatabase, users } = require('./express_server')
+
 // Generate random alphanumeric characters
 const randomGenerator = generateRandomString => {
   return Math.random().toString(36).substr(2, 6);
 }
 
-module.exports = { randomGenerator }
+const urlsForUser = function (user_id) {
+  const urlsBelongingToUser = {};
+  for (url in urlDatabase) {
+    if (user_id === urlDatabase[url].userID) {
+      urlsBelongingToUser[url] = urlDatabase[url];
+    }
+  }
+  return urlsBelongingToUser;
+}
+
+module.exports = { randomGenerator, urlsForUser }

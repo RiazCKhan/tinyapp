74550a678bd090c08e49cc34acb29beb8183a136
RiazK <RiazCKhan@gmail.com>
2021-09-06 12:56:26 -0400

    Added additional helper functions

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,50 @@
+const bcrypt = require('bcrypt');
+
+// ------ User & URL "Database" ------ \\
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "123"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.lighthouselabs.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
+
+// ------ HELPER FUNCTIONS ------ \\
+
 // Generate random alphanumeric characters
 const randomGenerator = generateRandomString => {
   return Math.random().toString(16).substr(2, 6);
 }
 
-module.exports = { randomGenerator }
+// URLs displayed for unique user
+const urlsForUser = function (user_id) {
+  const uniqueUserUrls = {};
+  for (url in urlDatabase) {
+    if (user_id === urlDatabase[url].userID) {
+      uniqueUserUrls[url] = urlDatabase[url];
+    }
+  }
+  return uniqueUserUrls;
+}
+
+const getUserWithEmail = (email, users) => {
+  for (let id in users) {
+    if(users[id].email === email) {
+      return users[id]
+    }
+  }
+  return null
+}
+
+
+module.exports = { urlDatabase, users, randomGenerator, urlsForUser, getUserWithEmail }

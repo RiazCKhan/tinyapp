25a11a1c01cfe9f6a395218f3fc9408622b87581
Riaz C Khan <86076827+RiazCKhan@users.noreply.github.com>
2021-09-06 13:27:31 -0400

    Delete functionAid.js

--- functionAid.js
+++
@@ -1,6 +0,0 @@
-// Generate random alphanumeric characters
-const randomGenerator = generateRandomString => {
-  return Math.random().toString(16).substr(2, 6);
-}
-
-module.exports = { randomGenerator }
